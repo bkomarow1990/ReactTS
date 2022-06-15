c118ba004c75e7e1a138035ca9b43b25b90bc8cb
Annonymous <dev@rivne.itstep.org>
2022-06-15 20:53:35 +0300

    Init

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import LoginPage from "./auth/login";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import HomePage from "./components/home";
+import HomeLayout from "./containers/homeLayout";
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Routes>
+          <Route path="/" element={<HomeLayout/>}></Route>
+          <Route index element={<HomePage/>}></Route>
+          <Route path="login" element={<LoginPage/>}></Route>
+        </Routes>
     </div>
   );
 }
---
+++ src/containers/homeLayout/index.tsx
@@ -0,0 +1,16 @@
+import { Outlet } from "react-router";
+import Navbar from "./Navbar";
+
+const HomeLayout = () => {
+    return (
+        <>
+        <Navbar />
+        <main>
+            <div className="container">
+                <Outlet />
+            </div>
+        </main>
+        </>
+    );
+}
+export default HomeLayout;
