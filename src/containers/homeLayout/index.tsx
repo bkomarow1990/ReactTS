9ba963744fe23cf4387ded3160b57e98e0197aba
Bogdan <71147719+bkomarow1990@users.noreply.github.com>
2022-07-02 14:26:13 +0300

    ReCaptcha v3 (not completed)

--- src/containers/homeLayout/index.tsx
+++ src/containers/homeLayout/index.tsx
@@ -1,16 +1,19 @@
 import * as React from 'react';
+import { GoogleReCaptchaProvider } from 'react-google-recaptcha-v3';
 import { Outlet } from "react-router";
 import Navbar from "./Navbar";
 
 const HomeLayout = () => {
     return (
         <>
+        <GoogleReCaptchaProvider>
         <Navbar />
         <main>
             <div className="container">
                 <Outlet />
             </div>
         </main>
+        </GoogleReCaptchaProvider>
         </>
     );
 }
